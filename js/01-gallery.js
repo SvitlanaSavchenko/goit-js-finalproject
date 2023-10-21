ffc0acf267147494300c6b5c7ce07e8480d2ac03
SvitlanaSavchenko <134533907+SvitlanaSavchenko@users.noreply.github.com>
2023-10-21 08:48:07 +0200

    homework-done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+// Отримання посилання на елемент ul.gallery
+const galleryList = document.querySelector(".gallery");
+
+// Рендер розмітки галереї
+function renderGalleryItems(items) {
+  const galleryMarkup = items
+    .map(
+      (item, index) => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img
+            class="gallery__image"
+            src="${item.preview}"
+            data-source="${item.original}"
+            data-index="${index}"
+            alt="${item.description}"
+          />
+        </a>
+      </li>
+    `
+    )
+    .join("");
+
+  galleryList.innerHTML = galleryMarkup;
+}
+
+// Обробник кліку на галереї
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+
+  // Отримання URL великого зображення
+  const largeImageURL = basicLightbox.create(
+    `<img class="gallery__image-big" src="${event.target.dataset.source}">`,
+    // Відкриття модального вікна
+    {
+      onShow: () => window.addEventListener("keydown", onKeydownEsc),
+      onClose: () => window.removeEventListener("keydown", onKeydownEsc),
+    }
+  );
+
+  const onKeydownEsc = (event) => {
+    if (event.code === "Escape") largeImageURL.close();
+  };
+
+  largeImageURL.show();
+}
+
+// Додавання обробника кліку для галереї
+galleryList.addEventListener("click", onGalleryItemClick);
+
+// Рендер галереї при завантаженні сторінки
+window.addEventListener("load", () => {
+  renderGalleryItems(galleryItems);
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+// Отримання посилання на елемент ul.gallery
+const galleryList = document.querySelector(".gallery");
+
+// Рендер розмітки галереї
+function createMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) => `
+        <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+          </a>
+        </li>
+      `
+    )
+    .join("");
+}
+
+// Вставка створеної розмітки в елемент ul.gallery
+galleryList.innerHTML = createMarkup(galleryItems);
+
+// Підключення скрипту і стилів бібліотеки SimpleLightbox (CDN)
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt", // Використовувати атрибут 'alt' як підпис
+  captionDelay: 250, // Затримка перед показом підпису
+});
+
+// Рендер галереї при завантаженні сторінки
+window.addEventListener("load", () => {
+  createMarkup(galleryItems);
+});
